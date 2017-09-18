7d999ba2a8563b14479b25da4a208ed523f4d40a
luisa86 <lfsh86@gmail.com>
2017-09-18 19:56:16 +0200

    refactorizado

--- controllers/RecipeController.js
+++ controllers/RecipeController.js
@@ -1,5 +1,11 @@
 module.exports = {
   index: (req, res, next) => {
     res.render('recipe')
+  },
+  post_new: (req, res, next) => {
+    res.render(index)
   }
+
+
+
 }
--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,6 @@ const RecipeController = require('../controllers/RecipeController')
 
 router.get('/', IndexController.index)
 router.get('/recipe', RecipeController.index)
+router.post('/recipe/new', RecipeController.post_new)
 
 module.exports = router
