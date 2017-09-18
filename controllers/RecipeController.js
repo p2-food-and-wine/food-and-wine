89f84dfee5dbd690a559c9385dd14aa96529ad6a
luisa86 <lfsh86@gmail.com>
2017-09-18 22:35:07 +0200

    hecha parte de route de recipe

--- controllers/RecipeController.js
+++ controllers/RecipeController.js
@@ -3,9 +3,51 @@ module.exports = {
     res.render('recipe')
   },
   post_new: (req, res, next) => {
-    res.render(index)
-  }
+    const newRecipe= new Recipe({
+      name       : req.body.name,
+      type       : req.body.type,
+      description: req.body.description,
+      ingredient : req.body.ingredient,
+      avatar     : req.body.avatar
+    })
+    newRecipe.save((err) => {
+      if (err) {
+        return err;
+      } else {
+        return res.redirect("/recipe");
+      }
+    });
+  },
+
+  delete:(req, res, next)=>{
+    Recipe.findByIdAndRemove(req.params.id, (err, obj) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/recipe");
+    });
+  },
 
 
+  editGet: (req, res, next) => {
+    Recipe.findById(req.params.id, (err, recipe) => {
+      if (err) {
+        console.log(err);
+      }
+      res.render('recipes/update', {
+        recipe: recipe
+      });
+    });
+  },
 
+  editPost: (req, res, next) => {
+    const { name, type, description, ingredient, avatar } = req.body
+    const updates = { name, type, description, ingredient, avatar }
+    Recipe.findByIdAndUpdate(req.params.id, updates, (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      res.redirect(`/recipe/${result._id}`);
+    });
+  },
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,12 @@
-const router = require('express').Router()
-const IndexController = require('../controllers/IndexController')
+const router           = require('express').Router()
+const IndexController  = require('../controllers/IndexController')
 const RecipeController = require('../controllers/RecipeController')
 
 router.get('/', IndexController.index)
 router.get('/recipe', RecipeController.index)
 router.post('/recipe/new', RecipeController.post_new)
+router.get('/:id/delete', RecipeController.delete)
+router.get('/:id/update', RecipeController.editGet)
+router.post('/:id/update',RecipeController.editPost)
 
 module.exports = router
