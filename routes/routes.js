3acc8d31627ac677342bce1503939bd7d7e93c9e
luisa86 <lfsh86@gmail.com>
2017-09-19 20:21:42 +0200

    subir imagen

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,15 @@
 const router = require('express').Router()
 const IndexController = require('../controllers/IndexController')
 const RecipeController = require('../controllers/RecipeController')
+const multer  = require('multer')
+const image= multer({ dest: './public/images/' })
 
 router.get('/', IndexController.index)
 
 router.get('/recipes', RecipeController.index)
 
 router.get('/recipes/new', RecipeController.new)
-router.post('/recipes/create', RecipeController.create)
+router.post('/recipes/create', image.single('avatar'), RecipeController.create)
 
 router.get('/recipes/:id', RecipeController.show)
 
