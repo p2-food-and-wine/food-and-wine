eef96eda3729a0e7bae5c51ff3544bf9d913d048
luisa86 <lfsh86@gmail.com>
2017-09-19 17:09:17 +0200

    cambio de rutas

--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,19 @@
-const router           = require('express').Router()
-const IndexController  = require('../controllers/IndexController')
+const router = require('express').Router()
+const IndexController = require('../controllers/IndexController')
 const RecipeController = require('../controllers/RecipeController')
 
 router.get('/', IndexController.index)
 
-router.get('/recipe', RecipeController.index)
+router.get('/recipes', RecipeController.index)
 
-router.get('/recipes/new', RecipeController.get_new)
-router.post('/recipes/new', RecipeController.post_new)
+router.get('/recipes/new', RecipeController.new)
+router.post('/recipes/create', RecipeController.create)
 
-router.get('/recipes/list', RecipeController.get_list)
+router.get('/recipes/:id', RecipeController.show)
+
+router.get('/recipes/:id/edit', RecipeController.edit)
+router.post('/recipes/:id', RecipeController.update)
 
 router.get('/recipes/:id/delete', RecipeController.delete)
-
-router.get('/recipes/:id/edit', RecipeController.editGet)
-router.post('/recipes/:id/edit',RecipeController.editPost)
 
 module.exports = router
