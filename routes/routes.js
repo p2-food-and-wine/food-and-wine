fe844aa64ac2d0ef9feda3bffee58c6b861345be
Davix76 <30471850+Davix76@users.noreply.github.com>
2017-09-22 12:27:26 +0200

    Before shitstorm

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const IndexController = require('../controllers/IndexController')
 const RecipeController = require('../controllers/RecipeController')
 const WineController = require('../controllers/WineController')
-router.get('/', IndexController.index)
+
 router.get('/recipe', RecipeController.index)
 router.get('/wines', WineController.index)
 
